6c5b66ad799bfaf31336f5f552d51c0da148872d
hullabaloo-vincent <vincega13@gmail.com>
2020-07-29 19:32:55 -0400

    Added JTextArea and other fixes
    Parameterized JList fileArchiveList; Added uneditable JTextArea to right split pane

--- OCR/src/OCR_App.java
+++ OCR/src/OCR_App.java
@@ -3,16 +3,15 @@ import java.io.File;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
-
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import com.hullabaloo.gui.AppWindow;
@@ -24,35 +23,38 @@ import net.sourceforge.tess4j.TesseractException;
 
 public class OCR_App {
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException,IllegalAccessException, UnsupportedLookAndFeelException {
+        
         /*---GUI INIT---*/
-
         JFrame window =  new AppWindow("Document Analyzer", 800, 700);
  
+        //File buttons
         JMenuBar menu = new JMenuBar();
         JMenu menuFile = new JMenu("File");
         JMenuItem buttonChooseDirectory = new JMenuItem("Choose Directory");
         JMenuItem buttonInterpret = new JMenuItem("Interpret Documents");
-
         menu.add(menuFile);
         menuFile.add(buttonChooseDirectory);
         menuFile.add(buttonInterpret);
 
-
+        //List of found files
         final DefaultListModel<String> fileArchive = new DefaultListModel<>(); //Holds the locations of the files
-        final JList fileArchiveList = new JList(fileArchive); //Displays model1
-
+        final JList<String> fileArchiveList = new JList<String>(fileArchive); //Displays model1
         final JScrollPane scroll = new JScrollPane(fileArchiveList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
         JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED); //Scrollbar for model1
 
-        JPanel rightPanel = new JPanel();
+        JTextArea fileContents = new JTextArea();
+        fileContents.setEditable(false);
 
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,scroll, rightPanel);
+        //Display the JList with the name and the content displayed in the JTextArea
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,scroll, fileContents);
         splitPane.setOneTouchExpandable(true);
         splitPane.setDividerLocation(200);
 
+        //Set window components
         window.setJMenuBar(menu);
         window.add(splitPane);
 
+        //Refresh the window after adding new components
         window.invalidate();
         window.validate();
         window.repaint();
