08bf5406d5bac157899a4df861f4b4fb8398d8f1
hullabaloo-vincent <vincega13@gmail.com>
2020-07-29 22:51:31 -0400

    Functionality Update
    You can now click on JList items and have it load the corresponding text content

--- OCR/src/OCR_App.java
+++ OCR/src/OCR_App.java
@@ -1,9 +1,13 @@
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.awt.event.ActionEvent;
 
 import javax.swing.DefaultListModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JMenuItem;
@@ -22,12 +26,16 @@ import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 public class OCR_App {
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException,IllegalAccessException, UnsupportedLookAndFeelException {
-        
+
+    static boolean hasInterpretedDocs = false;
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
+            IllegalAccessException, UnsupportedLookAndFeelException {
+
         /*---GUI INIT---*/
-        JFrame window =  new AppWindow("Document Analyzer", 800, 700);
- 
-        //File buttons
+        JFrame window = new AppWindow("Document Analyzer", 800, 700);
+
+        // File buttons
         JMenuBar menu = new JMenuBar();
         JMenu menuFile = new JMenu("File");
         JMenuItem buttonChooseDirectory = new JMenuItem("Choose Directory");
@@ -36,25 +44,28 @@ public class OCR_App {
         menuFile.add(buttonChooseDirectory);
         menuFile.add(buttonInterpret);
 
-        //List of found files
-        final DefaultListModel<String> fileArchive = new DefaultListModel<>(); //Holds the locations of the files
-        final JList<String> fileArchiveList = new JList<String>(fileArchive); //Displays model1
+        // List of found files
+        final DefaultListModel<String> fileArchive = new DefaultListModel<>(); // Holds the locations of the files
+        final JList<String> fileArchiveList = new JList<String>(fileArchive); // Displays model1
         final JScrollPane scroll = new JScrollPane(fileArchiveList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED); //Scrollbar for model1
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
         JTextArea fileContents = new JTextArea();
         fileContents.setEditable(false);
+        fileContents.setWrapStyleWord(true);
+        final JScrollPane contentScroll = new JScrollPane(fileContents, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-        //Display the JList with the name and the content displayed in the JTextArea
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,scroll, fileContents);
+        // Display the JList with the name and the content displayed in the JTextArea
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, contentScroll);
         splitPane.setOneTouchExpandable(true);
         splitPane.setDividerLocation(200);
 
-        //Set window components
+        // Set window components
         window.setJMenuBar(menu);
         window.add(splitPane);
 
-        //Refresh the window after adding new components
+        // Refresh the window after adding new components
         window.invalidate();
         window.validate();
         window.repaint();
@@ -62,35 +73,51 @@ public class OCR_App {
         /*---END OF GUI INIT---*/
 
         final ArrayList<String> foundFiles_SOURCE = new ArrayList<String>();
+        final ArrayList<String> foundFiles_NAME = new ArrayList<String>();
 
-        Tesseract tesseract = new Tesseract(); 
-        
+        Dictionary<String, String> interpretedDocs = new Hashtable<String, String>();
+
+        Tesseract tesseract = new Tesseract();
+
         buttonChooseDirectory.addActionListener((ActionEvent e) -> {
-            createFileChooser fileChooser = new createFileChooser("Select a starting location", "FILES_AND_DIRECTORIES");
+            createFileChooser fileChooser = new createFileChooser("Select a starting location",
+                    "FILES_AND_DIRECTORIES");
             directorySearch ds = new directorySearch(fileChooser.getSelection());
 
-            for (int fileIndex = 0; fileIndex < ds.getAllFileNames().size()-1; fileIndex++){
+            for (int fileIndex = 0; fileIndex < ds.getAllFileNames().size() - 1; fileIndex++) {
                 fileArchive.addElement(ds.getAllFileNames().get(fileIndex));
+                // foundFiles.put(ds.getAllFileNames().get(fileIndex),ds.getAllFilesSource().get(fileIndex));
                 foundFiles_SOURCE.add(ds.getAllFilesSource().get(fileIndex));
+                foundFiles_NAME.add(ds.getAllFileNames().get(fileIndex));
             }
         });
 
         buttonInterpret.addActionListener((ActionEvent e) -> {
-            try { 
-                String programPath = System.getProperty("user.dir")+"/tessdata";
+            try {
+                String programPath = System.getProperty("user.dir") + "/tessdata";
                 programPath = programPath.replace("\\", "/");
-                tesseract.setDatapath(programPath); 
-                
-                for (int fileIndex = 0; fileIndex < foundFiles_SOURCE.size(); fileIndex++){
-                    String text 
-                        = tesseract.doOCR(new File(foundFiles_SOURCE.get(fileIndex))); 
-    
-                    System.out.print(text); 
+                tesseract.setDatapath(programPath);
+
+                for (int fileIndex = 0; fileIndex < foundFiles_SOURCE.size(); fileIndex++) {
+                    String text = tesseract.doOCR(new File(foundFiles_SOURCE.get(fileIndex)));
+
+                    interpretedDocs.put(foundFiles_NAME.get(fileIndex), text);
                 }
-            } 
-            catch (TesseractException TE) { 
+                hasInterpretedDocs = true;
+            } catch (TesseractException TE) { 
                 TE.printStackTrace(); 
-            } 
+            }
+        });
+
+        fileArchiveList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent arg0) {
+                if (!arg0.getValueIsAdjusting()) {
+                    if (hasInterpretedDocs){
+                        fileContents.setText(interpretedDocs.get(fileArchiveList.getSelectedValue().toString()));
+                    }
+                }
+            }
         });
     }
 } 
