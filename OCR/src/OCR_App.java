135a9e200195fdf0d8d358130086561fa696e9d7
hullabaloo-vincent <vincega13@gmail.com>
2020-08-01 09:01:55 -0400

    Cleaned up GUI init

--- OCR/src/OCR_App.java
+++ OCR/src/OCR_App.java
@@ -33,55 +33,34 @@ public class OCR_App {
     static boolean hasInterpretedDocs = false;
     static final DefaultListModel<String> calcJobs = new DefaultListModel<>();
     static final JList<String> calcJobsList = new JList<String>(calcJobs);
+
+    static final DefaultListModel<String> fileArchive = new DefaultListModel<>();
+    static final JList<String> fileArchiveList = new JList<String>(fileArchive);
+
     static Dictionary<String, String> interpretedDocs = new Hashtable<String, String>();
     static final ArrayList<String> foundFiles_SOURCE = new ArrayList<String>();
     static final ArrayList<String> foundFiles_NAME = new ArrayList<String>();
+
+    static JMenuBar menu;
+    static JMenu menuFile;
+    static JMenuItem buttonChooseDirectory;
+    static JMenuItem buttonInterpret;
+    static JProgressBar jobProgress;
+    static JPanel jobPanel;
+    static JSplitPane contentJobPane;
+    static JSplitPane splitPane;
+
+    static JTextArea fileContents;
+    static JScrollPane contentScroll;
+    static JScrollPane scroll;
+    static JScrollPane jobScroll;
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
             IllegalAccessException, UnsupportedLookAndFeelException {
 
         /*---GUI INIT---*/
         JFrame window = new AppWindow("Document Analyzer", 800, 700);
-
-        /*---FILE BUTTONS---*/
-        JMenuBar menu = new JMenuBar();
-        JMenu menuFile = new JMenu("File");
-        JMenuItem buttonChooseDirectory = new JMenuItem("Choose Directory");
-        JMenuItem buttonInterpret = new JMenuItem("Interpret Documents");
-        menu.add(menuFile);
-        menuFile.add(buttonChooseDirectory);
-        menuFile.add(buttonInterpret);
-
-        /*---LIST FOUND FILES---*/
-        final DefaultListModel<String> fileArchive = new DefaultListModel<>(); // Holds the locations of the files
-        final JList<String> fileArchiveList = new JList<String>(fileArchive); // Displays model1
-        final JScrollPane scroll = new JScrollPane(fileArchiveList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-        /*---LIST JOBS FILES---*/
-        final JScrollPane jobScroll = new JScrollPane(calcJobsList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-        JTextArea fileContents = new JTextArea();
-        fileContents.setEditable(false);
-        fileContents.setWrapStyleWord(true);
-        final JScrollPane contentScroll = new JScrollPane(fileContents, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-        JProgressBar jobProgress = new JProgressBar(0,10);
-        JPanel jobPanel = new JPanel();
-        jobPanel.setLayout(new GridLayout(2, 0)); 
-        jobPanel.add(BorderLayout.NORTH, jobScroll);
-        jobPanel.add(BorderLayout.SOUTH,jobProgress);
-
-        JSplitPane contentJobPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, contentScroll, jobPanel);
-        contentJobPane.setDividerLocation(500);
-
-        // Display the JList with the name and the content displayed in the JTextArea
-        
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, contentJobPane);
-        splitPane.setOneTouchExpandable(true);
-        splitPane.setDividerLocation(200);
+        initComp();
 
         // Set window components
         window.setJMenuBar(menu);
@@ -114,7 +93,7 @@ public class OCR_App {
             programPath = programPath.replace("\\", "/");
 
             tesseract.setDatapath(programPath);
-            
+
             SwingWorker worker = new SwingWorker<String, Void>() {
                 @Override
                 public String doInBackground() {
@@ -130,11 +109,11 @@ public class OCR_App {
                         }
                         interpretedDocs.put(foundFiles_NAME.get(fileIndex), text);
                         calcJobs.set(fileIndex, "Completed: " + foundFiles_NAME.get(fileIndex));
-                        jobProgress.setValue(fileIndex+1);
+                        jobProgress.setValue(fileIndex + 1);
                     }
                     return "";
                 }
-            
+
                 @Override
                 public void done() {
                     System.out.println("DONE");
@@ -148,11 +127,47 @@ public class OCR_App {
             @Override
             public void valueChanged(ListSelectionEvent arg0) {
                 if (!arg0.getValueIsAdjusting()) {
-                    if (hasInterpretedDocs){
+                    if (hasInterpretedDocs) {
                         fileContents.setText(interpretedDocs.get(fileArchiveList.getSelectedValue().toString()));
                     }
                 }
             }
         });
+    }
+
+    public static void initComp() {
+        menu = new JMenuBar();
+        /*---FILE BUTTONS---*/
+        menuFile = new JMenu("File");
+        buttonChooseDirectory = new JMenuItem("Choose Directory");
+        buttonInterpret = new JMenuItem("Interpret Documents");
+        menu.add(menuFile);
+        menuFile.add(buttonChooseDirectory);
+        menuFile.add(buttonInterpret);
+
+        scroll = new JScrollPane(fileArchiveList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        jobScroll = new JScrollPane(calcJobsList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        fileContents = new JTextArea();
+        fileContents.setEditable(false);
+        fileContents.setWrapStyleWord(true);
+        contentScroll = new JScrollPane(fileContents, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        jobProgress = new JProgressBar(0, 10);
+        jobPanel = new JPanel();
+        jobPanel.setLayout(new GridLayout(2, 0));
+        jobPanel.add(BorderLayout.NORTH, jobScroll);
+        jobPanel.add(BorderLayout.SOUTH, jobProgress);
+
+        contentJobPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, contentScroll, jobPanel);
+        contentJobPane.setDividerLocation(500);
+
+        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, contentJobPane);
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setDividerLocation(200);
     }
 } 
